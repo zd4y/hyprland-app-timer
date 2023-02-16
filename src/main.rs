abf1e10cc0e6c8c57426471ebdf73dba4da13820
zd4y <59004538+zd4y@users.noreply.github.com>
2023-02-16 09:19:37 -0500

    fix

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ use log4rs::encode::pattern::PatternEncoder;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let (tx, rx) = mpsc::channel(100);
+    let (tx, mut rx) = mpsc::channel(100);
     ctrlc::set_handler(move || {
-        log::info!("received signal, terminating...");
+        log::info!("in ctrlc handler, sending signal...");
         tx.blocking_send(())
             .expect("Error sending signal to channel")
     })
@@ -30,45 +30,42 @@ async fn main() -> anyhow::Result<()> {
     log4rs::init_config(config)?;
 
     let pool = app_timer2::get_pool().await?;
-    run(&pool, rx).await?;
+    run(&pool, &mut rx).await?;
 
     Ok(())
 }
 
-async fn run(pool: &SqlitePool, mut rx: Receiver<()>) -> anyhow::Result<()> {
+async fn run(pool: &SqlitePool, rx: &mut Receiver<()>) -> anyhow::Result<()> {
     let mut focus_monitor = AsyncFocusMonitor::try_new()?;
 
     let mut now = Instant::now();
     let mut last_window: Option<focus_monitor::Window> = None;
     let mut records = vec![];
-    let mut count = 0;
 
     loop {
         tokio::select! {
             biased;
 
-            _ = rx.recv() => break,
-            window = focus_monitor.recv() => {
-                let new_last_window = window?;
+            _ = rx.recv() => {
+                log::info!("received signal, breaking loop...");
+                break
+            },
+            Ok(new_last_window) = focus_monitor.recv() => {
                 let duration = now.elapsed();
+                log::info!("received window");
+                log::info!("window={:#?}", new_last_window);
                 log::info!("{:?} ({:?})", last_window, duration);
-                if let Some(window) = last_window {
-                    records.push(app_timer2::Window {
-                        duration,
-                        datetime: Utc::now() - chrono::Duration::from_std(duration).unwrap(),
-                        title: window.title,
-                        class: window.class,
-                    });
-                    count+=1;
+                if let Some(window) = last_window.take() {
+                    records.push(new_window(window, duration));
                 }
                 now = Instant::now();
                 last_window = new_last_window;
 
-                if count > 0 && count % 1000 == 0 {
+                if records.len() >= 1000 {
                     save_windows(pool, &mut records).await?;
                 }
-            },
-        };
+            }
+        }
     }
 
     // Add the latest window
@@ -76,15 +73,12 @@ async fn run(pool: &SqlitePool, mut rx: Receiver<()>) -> anyhow::Result<()> {
         log::info!("last window: {:?}", window);
         log::info!("records before adding last window {:?}", records);
         let duration = now.elapsed();
-        records.push(app_timer2::Window {
-            datetime: Utc::now() - chrono::Duration::from_std(duration).unwrap(),
-            title: window.title,
-            class: window.class,
-            duration,
-        })
+        records.push(new_window(window, duration))
     }
 
     save_windows(pool, &mut records).await?;
+
+    log::info!("exiting program, bye");
 
     Ok(())
 }
@@ -97,4 +91,13 @@ async fn save_windows(
     log::info!("windows saved: {:?}", windows);
     windows.clear();
     Ok(())
+}
+
+fn new_window(window: focus_monitor::Window, duration: std::time::Duration) -> app_timer2::Window {
+    app_timer2::Window {
+        datetime: Utc::now() - chrono::Duration::from_std(duration).unwrap(),
+        title: window.title,
+        class: window.class,
+        duration,
+    }
 }
